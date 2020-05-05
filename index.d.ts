91b876013c39d62cebc3a2b63f07fbf4d0b2fc97
Joepake <thuc.nguyentruong@gmail.com>
2020-05-05 17:12:46 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -9,7 +9,7 @@ declare module "react-native-easy-toast" {
     show: (
       text: string | ReactNode,
       duration?: number,
-      color?: string,
+      backgroundColor?: string,
       callback?: (() => void)
     ) => void;
     close: (duration?: number) => void;
